11008d9ec82c24747424a35cefa4ca9db4f3a487
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2022-11-06 21:16:38 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-       
+
 class NFL extends Array {
  constructor(name, ...hometeams) {
-     
- }
-}
-
-const NFLteam = new NFL('addgame'); 'date', 'Teams Rival('awayteam')'/name'/'hometeam');
-     {name: 'Arizona Cardinal', date: 'January 10', hometeam: 'Cardinal', awayteam: ' Atlanta Falcons' +class NFL extends Array {
- constructor(name, ...hometeams) {
-     
  }
 }
 
@@ -21,13 +13,13 @@ const NFLteam = new NFL('addgame'); 'date', 'Teams Rival('awayteam')'/name'/'hom
    {name: 'Detroit Lions ', date: 'January 10', hometeam: 'Lions', awayteam: '  Atlanta Falcons',  awaypoint: (5 points)'; homepoint: '9', away:'(10points)' },      
    {name: 'Jacksonville Jaguars', date: 'January 10', hometeam: 'Jaguars', awayteam: ' Dallas Cowboys  ', awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },       
    {name: 'Green Bay Packers  ', date: 'January 10', hometeam: 'Packers', awayteam: ' Jacksonville Jaguars ',  'awaypoint: (6 points)'; homepoint: '9', away:'(10points)' } ,         
-   {name: 'Los Angeles Rams', date: 'January 10', hometeam: 'Rams', awayteam: ' Jacksonville Jaguars ', awaypoint: (8 points)'; homepoint: '9', away:'(10points)' } ,             
+{name: 'Los Angeles Rams', date: 'January 10', hometeam: 'Rams', awayteam: ' Jacksonville Jaguars ', awaypoint: (8 points)'; homepoint: '9', away:'(10points)' } ,             
 {name: 'Minnesota Vikings ', date: 'January 10', hometeam: 'Cardinal', awayteam: ' Green Bay Packers ', awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },            
 {name: 'New Orleans Saints ', date: 'January 10', hometeam: 'Vikings', awayteam: ' Jacksonville Jaguars ' ,  awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },           
  {name: 'New York Giants', date: 'January 10', hometeam: 'Giants', awayteam: 'Minnesota Vikings ' , awaypoint: (6 points)'; homepoint: '16', away:'(10points)' },            
  {name: 'Denver Broncos ', date: 'January 10', hometeam: 'Broncos', awayteam: ' New Orleans Saints  ' ,awaypoint: (7 points)'; homepoint: '9', away:'(10points)' },             
    {name:'Houston Texan', date: 'January 10', hometeam: 'Texan', awayteam: 'Jacksonville Jaguars', awaypoint: (6 points)'; homepoint: '67', away:'(10points)' },
-      {name:'Miami Dolphins', date: 'January 10', hometeam: 'Dolphin', awayteam: 'Denver Broncos',awaypoint: (8 points)'; homepoint: '90', away:'(10points)' },
+{name:'Miami Dolphins', date: 'January 10', hometeam: 'Dolphin', awayteam: 'Denver Broncos',awaypoint: (8 points)'; homepoint: '90', away:'(10points)' },
  {name:'Tennessee Titans', date: 'January 10', hometeam: 'Titans', awayteam: 'Miami Dolphins ', awaypoint: (12 points)'; homepoint: '9', away:'(10points)' },
    {name:'Buffalo Bills', date: 'January 10', hometeam: 'Bills', awayteam: 'Miami Dolphins ', awaypoint: (12 points)'; homepoint: '9', away:'(10points)' },
    
@@ -44,48 +36,23 @@ function myFunction() {
   document.getElementById("addgameOutput").innerHTML =
  "My date is" + myDate + ".<br> My Hometeam is  " + myHometeam + ".<br>MyAwayteam is" + myAwayteam + ".<br>My Homepoint"+ myHomepoint +
     ".<br>MyAway is"+ myAway + ".";
-
-}
-        
-      }
-    }
-  }
-}', 
-       awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },
-     {name: 'Atlanta Falcons', date: 'January 10', hometeam: 'Falcons', awayteam: ' Jacksonville Jaguars ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ',awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },
-     {name: 'Carolina Panthers',date: 'January 10', hometeam: 'Panthers', awayteam: ' Jacksonville Jaguars ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ', awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },
-    {name: 'Chicago Bears ', date: 'January 10', hometeam: 'Bears', awayteam: ' Carolina Panthers ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ',awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },
-    {name: 'Dallas Cowboys', date: 'January 10', hometeam: 'Cowboys', awayteam: ' Carolina Panthers ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ', awaypoint: (7 points)'; homepoint: '9', away:'(10points)' },
-   {name: 'Detroit Lions ', date: 'January 10', hometeam: 'Lions', awayteam: '  Atlanta Falcons' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ',  awaypoint: (5 points)'; homepoint: '9', away:'(10points)' },      
-   {name: 'Jacksonville Jaguars', date: 'January 10', hometeam: 'Jaguars', awayteam: ' Dallas Cowboys  ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ', awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },       
-   {name: 'Green Bay Packers  ', date: 'January 10', hometeam: 'Packers', awayteam: ' Jacksonville Jaguars ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png',  'awaypoint: (6 points)'; homepoint: '9', away:'(10points)' } ,         
-   {name: 'Los Angeles Rams', date: 'January 10', hometeam: 'Rams', awayteam: ' Jacksonville Jaguars ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ', awaypoint: (8 points)'; homepoint: '9', away:'(10points)' } ,             
-{name: 'Minnesota Vikings ', date: 'January 10', hometeam: 'Cardinal', awayteam: ' Green Bay Packers ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ', awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },            
-{name: 'New Orleans Saints ', date: 'January 10', hometeam: 'Vikings', awayteam: ' Jacksonville Jaguars ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ' ,  awaypoint: (6 points)'; homepoint: '9', away:'(10points)' },           
- {name: 'New York Giants', date: 'January 10', hometeam: 'Giants', awayteam: 'Minnesota Vikings ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ' , awaypoint: (6 points)'; homepoint: '16', away:'(10points)' },            
- {name: 'Denver Broncos ', date: 'January 10', hometeam: 'Broncos', awayteam: ' New Orleans Saints  ' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png  ' ,awaypoint: (7 points)'; homepoint: '9', away:'(10points)' },             
-   {name:'Houston Texan', date: 'January 10', hometeam: 'Texan', awayteam: 'Jacksonville Jaguars' + imagurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png ', awaypoint: (6 points)'; homepoint: '67', away:'(10points)' },
-      {name:'Miami Dolphins', date: 'January 10', hometeam: 'Dolphin', awayteam: 'Denver Broncos ' + imagurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png ',awaypoint: (8 points)'; homepoint: '90', away:'(10points)' },
- {name:'Tennessee Titans', date: 'January 10', hometeam: 'Titans', awayteam: 'Miami Dolphins ' + imagurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png ', awaypoint: (12 points)'; homepoint: '9', away:'(10points)' },
-   {name:'Buffalo Bills', date: 'January 10', hometeam: 'Bills', awayteam: 'Bill' + imgurl: 'https://i.postimg.cc/bNL4hvy7/1vl-Eqqoyb9u-Tq-BYi-Be-NH-w-48x48.png ', awaypoint: (12 points)'; homepoint: '9', away:'(10points)' },
-   
-        'document.getElementById("gameButton").addEventListener("click", myFunction);
-
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction() {
-  let mMyDate = document.getElementById("date").value;
-  let MyHometeam = document.getElementById("hometeam").value;
-    let MyAwayteam = document.getElementById("awayteam").value;
-  let MyHomepoint = document.getElementById("homepoint").value; 
-    let MyAway = document.getElementById("away").value;
-  
-  //const result = divValue.textContent.trim(); alert(result)
-  document.getElementById("addgameOutput").innerHTML =
- "My date is" + myDate + ".<br> My Hometeam is  " + myHometeam + ".<br>MyAwayteam is" + myAwayteam + ".<br>My Homepoint"+ myHomepoint +
-    ".<br>MyAway is"+ myAway + ".";
-
+  document.getElementById("myDropdown").classList.toggle("show");
 }
-        
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show')       
       }
     }
   }
-}
+}' 
+
