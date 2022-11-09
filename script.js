beed95b8b55539ff5ddb6fa98bba93feb3d98162
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2022-11-09 15:09:56 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -261,5 +261,72 @@ var temp = document.querySelector('.date');
     var content = "<b> Games Entered by User</b><br>";
     document.getElementById("gameOutput").innerHTML = content;
   }
+  function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+
+function addTable() {
+
+/* Find a <table> element with id="myTable":*/
+var table = document.getElementById("myTable");
+
+/* Create an empty <tr> element and add it to the 1st position of the table:*/
+var row = table.insertRow(1);
+
+/*sert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:*/
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+/* Add some text to the new cells:*/
+cell1.innerHTML = document.getElementById("firstname").value;
+cell2.innerHTML = document.getElementById("lastname").value;
+  
+ /* alert("cell2:  "+cell2.innerHTML);*/
+  cell3.innerHTML = document.getElementById("grade").value;
+}
+  
+function resetTable() {
+
+/* Find a <table> element with id="myTable":*/
+var table = document.getElementById("myTable");
+
+/* Add some text to the new cells:*/
+ document.getElementById("firstname").value='';
+ document.getElementById("lastname").value='';
+document.getElementById("grade").value='';
+}
+  
 }
 
