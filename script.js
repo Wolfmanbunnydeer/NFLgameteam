3e30ea25a0dcc3d7aaad14c2564af67411192460
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2022-11-11 01:14:33 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,16 +3,175 @@ function myFunction() {
   document.getElementById("GameButton").addEventListener("click", myFunction);
 }
 // create NFL = document.createElement('result').innerHTML;
-function onloadData(){
-}
-var temp = document.querySelector('.date');
-  var Hometeambutton = document.querySelector(."Hometeam");
-  var awayteam = document.querySelector(".awayteam");
-  var homepoint = document.querySelector(".homepoint");
-  var awaypointDiv = document.querySelector(".awaypoint");
 
- 
-  function myFunction() {
+function DisplayData() {
+  var NFL = (document.createElement("game").innerHTML = [
+    {
+      City: "Arizona",
+      Name: "Arizona Cardinal",
+      Date: "March 7",
+      AwayTeam: "Falcon",
+      Hometeam: "Cardinals",
+      Homepoint: "78",
+      AwayPoint: "45"
+    },
+    {
+      City: "Atlanta",
+      Name: "Falcons",
+      Date: "March 7",
+      AwayTeam: "Minnesota Vikings",
+      Hometeam: "Falcons",
+      Homepoint: "23",
+      AwayPoint: "8"
+    },
+    {
+      City: "N/S Carolinas",
+      Name: "Carolina Panther",
+      Date: "March 7",
+      AwayTeam: "Minnesota Vikings",
+      Hometeam: "Panther",
+      Homepoint: "45",
+      AwayPoint: "8"
+    },
+    {
+      City: "Chicago",
+      Name: "Chicago Bears",
+      Date: "March 7",
+      AwayTeam: "Dallas Cowboys",
+      Hometeam: "Chicago Bears",
+      Homepoint: "78",
+      AwayPoint: "8"
+    },
+    {
+      City: "Dallas",
+      Name: "Dallas Cowboys",
+      Date: "March 7",
+      AwayTeam: "Chicago Bears",
+      Hometeam: "Dallas Cowboys",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Detroits",
+      Name: "Detroit Lions",
+      Date: "March 7",
+      AwayTeam: "Chicago Bears",
+      Hometeam: "Detroit Lions",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Jacksonville",
+      Name: "Jacksonville Jaguars",
+      Date: "March 7",
+      AwayTeam: "Chicago Bears",
+      Hometeam: "Jacksonville Jaguars",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+
+    {
+      City: "GreenBay",
+      Name: "Green Bay Packers",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "Green Bay Packers",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+
+    {
+      City: " Los Angeles",
+      Name: " Los Angeles Rams",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: " Los Angeles Rams",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: " Los Angeles",
+      Name: " Los Angeles Rams",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: " Los Angeles Rams",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Minnesota",
+      Name: "Minnesota Vikings",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "Minnesota",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "New Orleans",
+      Name: "New Orleans Saints",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "New Orleans Saints",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "New York",
+      Name: "New York Giants",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "New York Giants",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Denver",
+      Name: "Denver Broncos",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "Denver Broncos",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Houston ",
+      Name: "Houston Texan",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: "Houston Texan",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Miami ",
+      Name: " Miami Dolphins ",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: " Miami Dolphins ",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Tennessee",
+      Name: " Tennessee Titans",
+      Date: "March 7",
+      AwayTeam: "Detroit Lions",
+      Hometeam: " Tennessee Titans",
+      Homepoint: "90",
+      AwayPoint: "87"
+    },
+    {
+      City: "Buffalo",
+      Name: " Buffalo Bills",
+      Date: "March 7",
+      AwayTeam: "Miami Dolphins",
+      Hometeam: "Buffalo Bills",
+      Homepoint: "90",
+      AwayPoint: "87"
+    }
+  ]);
+  function AddGame() {
     let theDateName = document.getElementById("date").value;
     let theHomeTeam = document.getElementById("Hometeam").value;
 
@@ -39,7 +198,7 @@ var temp = document.querySelector('.date');
   }
   //Following codes are for  Movies
 
-  document.getElementById("gameButton").addEventListener("click", DateFunction);
+  document.getElementById("GameButton").addEventListener("click", DateFunction);
 
   function DateFunction() {
     let theDateseason = document.getElementById("games").value;
@@ -56,25 +215,31 @@ var temp = document.querySelector('.date');
   }
 
   function AddGame() {
-    var arrDate = newArray();
-    var arrHomeTeam = newArray();
-    var arrAwayTeam = newArray();
-    var arrHomePoint = newArray();
-    var arrAwayPoint = newArray();
-  }
+    var arrDate = new Array();
+    var arrHomeTeam = new Array();
+    var arrAwayTeam = new Array();
+    var arrHomePoint = new Array();
+    var arrAwayPoint = new Array();
     function AddGame() {
-      var arrDate = document.getElementById("date").innerHTML;
-      var arrHomeTeam = document.getElementById("Hometeam").innerHTML;
-      var arrAwayTeam = document.getElementById("AwayTeam").innerHTML;
-      var arrHomePoint = document.getElementById("HomePoint").innerHTML;
-      var arrAwayPoint = document.getElementById("awaypoint").innerHTML;    
+      var Date = document.getElementById("date").innerHTML;
+      var HomeTeam = document.getElementById("Hometeam").innerHTML;
+      var AwayTeam = document.getElementById("AwayTeam").innerHTML;
+      var HomePoint = document.getElementById("HomePoint").innerHTML;
+      var AwayPoint = document.getElementById("awaypoint").innerHTML;
+      function AddGame() {
+        arrDate[arrDate.length] = Date;
+        arrHomeTeam[arrHomeTeam.length] = HomeTeam;
+        arrAwayTeam[arrAwayTeam.length] = AwayTeam;
+        arrHomePoint[arrHomePoint.length] = HomePoint;
+        arrAwayPoint[arrAwayPoint.length] = AwayPoint;
       }
+
       document.getElementById("date").value = "";
       document.getElementById("Hometeam").value = "";
       document.getElementById("awayteam").value = "";
       document.getElementById("homepoint").value = "";
       document.getElementById("awaypoint").value = "";
-
+    }
 
     function AddGame() {
       var content = "<b>Data Entered by User :</b><br>";
@@ -86,183 +251,16 @@ var temp = document.querySelector('.date');
     }
     function AddGame() {
       document.getElementById("gameOutput").innerHTML = content;
-      arrDate = arrArray();
-      arrHomeTeam = arrArray();
-      arrHomePoint = arrArray();
-      arrAwayTeam = arrArray();
-      arrAwayPoint = arrArray();
+      arrDate = Array();
+      arrHomeTeam = Array();
+      arrHomePoint = Array();
+      arrAwayTeam = Array();
+      arrAwayPoint = Array();
     }
     var content = "<b> Games Entered by User</b><br>";
     document.getElementById("gameOutput").innerHTML = content;
-
-
- 
-function loadingdata() {
-
-/* Find a flex container element with class="flex-container":*/
-var data = document.getElementByClassName("flex-container");
-
-/* Add some text to the new cells:*/
- document.getElementById("date").value='';
- document.getElementById("HomeTeam").value='';
-document.getElementById("homepoint").value='';
-  document.getElementById("awayteam").value='';
-document.getElementById("awaypoint").value='';  
-  
+  }
 }
 
-
-// Create a "close" button and append it to each list item
-
-var myNodelist = document.getElementsByTagName("LI");
-
-var i;
-
-for (i = 0; i < myNodelist.length; i++) {
-
-  var span = document.createElement("SPAN");
-
-  var txt = document.createTextNode("\u00D7");
-
-  span.className = "close";
-
-  span.appendChild(txt);
-
-  myNodelist[i].appendChild(span);
-
-}
-
- 
-
-// Click on a close button to hide the current list item
-
-var close = document.getElementsByClassName("flex-container");
-
-var i;
-
-for (i = 0; i < close.length; i++) {
-
-  close[i].onclick = function() {
-
-    var div = this.parentElement;
-
-    div.style.display = "none";
-
-  }
-
-}
-
- 
-
-// Add a "checked" symbol when clicking on a list item
-
-var list = document.querySelector('ul');
-
-list.addEventListener('click', function(ev) {
-
-  if (ev.target.tagName === 'LI') {
-
-    ev.target.classList.toggle('checked');
-
-  }
-
-}, false);
-
- 
-
-// Create a new list item when clicking on the "Add" button
-
-function newElement() {
-
-  var li = document.createElement("li");
-
-  var inputValue = document.getElementById("item").value;
-
-  var t = document.createTextNode(inputValue);
-
-  li.appendChild(t);
-
-  if (inputValue === '') {
-
-    alert("This field cannot be empty!");
-
-  } else {
-
-    document.getElementById("list").appendChild(li);
-
-  }
-
-  document.getElementById("item").value = "";
-
- 
-
-  var span = document.createElement("SPAN");
-
-  var txt = document.createTextNode("\u00D7");
-
-  span.className = "close";
-
-  span.appendChild(txt);
-
-  li.appendChild(span);
-
- 
-
-  for (i = 0; i < close.length; i++) {
-
-    close[i].onclick = function() {
-
-      var div = this.parentElement;
-
-      div.style.display = "none";
-
-$(document).ready(function(){
-  var submitBtn = $('#submit-btn');
-  var inputs = $('.info');
-  var result = $('#result');
-  var dataHolder = {};
-  
-  submitBtn.click(function(e){
-    console.log(e); // Details about the event from clicking the button 
-    e.preventDefault(); // prevent the default behaviour of the button
-    $(inputs).each(function(i) {
-      console.log('All inputs in the form', $(this)); 
-      // $(this) refers to all of the inputs having the info Class.
-      // Knowing that, we could easally get their values or ther
-      // name attributes in order to create a meaningful and descriptive
-      // Object with the data
-      
-      console.log('The number if itterations', i);
-      // Knowing the number of ittereations(the number of inputs)
-      // We could assign i to be equal to the inputs name attribute.
-      // By doing that we can use i to assign the object key names,
-      // otherwise it would be 0: 1: 2: 3:
-      
-      i = $(this).attr('name'); // Assign i to the inputs name attributes
-      console.log('i is now changed to: ', i);
-      
-      // Add the value to the object
-      dataHolder[i] = $(this).val();
-    });
-    
-    // Now we need to output the result into the DOM result container
-    // We can do that by looping trough our dataHolder object
-    
-    for(var k in dataHolder) {
-      if(dataHolder.hasOwnProperty(k)) {
-        console.log('value of k ', k) // k is actually the key in the object
-        result.append('<p>' + k + ' : ' + dataHolder[k] + '</p>'); // Append the data in the result container
-      }
-    }
-  });
-});
-      
-      
-
-    }
-
-  }
-
-}
 
 
