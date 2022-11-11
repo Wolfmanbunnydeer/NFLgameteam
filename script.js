898dbfb0097f888fb0e1213027615a869f97d385
Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>
2022-11-11 18:39:56 -0500

    Update script.js
    Signed-off-by: Lougosi <83800145+Wolfmanbunnydeer@users.noreply.github.com>

--- script.js
+++ script.js
@@ -213,11 +213,13 @@ function AddGame() {
 }
 window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgames-team3/'; //Will take you to Github.
 window.open('https://wolfmanbunnydeer.github.io/NFLgames-team3/'); //This will open Google in a new window.
-}
 function newDoc() {
   window.location.assign("https://wolfmanbunnydeer.github.io/NFLgames-team3/")
+}   
 
-  }
   document.getElementById("Game").innerHTML = text;
-}
-   
+window.location.href = 'https://wolfmanbunnydeer.github.io/NFLgameteam/?'; //Will take you to Github.
+window.open('https://wolfmanbunnydeer.github.io/NFLgameteam/?'); //This will open Google in a new window.
+function newDoc() {
+  window.location.assign("https://wolfmanbunnydeer.github.io/NFLgames-team3/")
+}      
